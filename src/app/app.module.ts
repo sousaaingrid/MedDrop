d713acdf4afbf67339b7c556247745fec9aadfc2
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2023-06-15 16:52:05 -0300

    add recaptcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,7 +45,8 @@ import { StripeFailureComponent } from './components/stripe-failure/stripe-failu
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
